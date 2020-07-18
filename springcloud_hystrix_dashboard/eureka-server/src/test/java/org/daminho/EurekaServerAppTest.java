a1b383c41f4c564f1269d4ece64f33429703fd2f
damingho <damingho@163.com>
2020-07-18 17:38:07 +0800

    spring cloud research first commit

---
+++ springcloud_hystrix_dashboard/eureka-server/src/test/java/org/daminho/EurekaServerAppTest.java
@@ -0,0 +1,20 @@
+package org.daminho;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class EurekaServerAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
